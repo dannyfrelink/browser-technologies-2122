ab39d6185a477065f2e704851d46b86bda798e7f
dannyfrelink <danny-frelink@hotmail.com>
2022-03-30 13:59:39 +0200

    start home screen

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 6666;
+const port = 3000;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
 
 app.use((req, res) => {
     res.status(404).send('Sorry, deze pagina kon ik niet vinden.');
