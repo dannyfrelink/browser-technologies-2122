ee0f55ad1dd0a3cf4eb97a6ac325bca27689f8a8
dannyfrelink <danny-frelink@hotmail.com>
2022-03-30 14:52:28 +0200

    add success state

--- index.js
+++ index.js
@@ -7,31 +7,35 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.post('/wafs', (req, res) => {
     res.render('wafs');
-})
+});
 
 app.post('/css', (req, res) => {
     res.render('css');
-})
+});
 
 app.post('/pwa', (req, res) => {
     res.render('pwa');
-})
+});
 
 app.post('/bt', (req, res) => {
     res.render('bt');
-})
+});
 
 app.post('/rtw', (req, res) => {
     res.render('rtw');
-})
+});
 
 app.post('/hcd', (req, res) => {
     res.render('hcd');
-})
+});
+
+app.post('/success', (req, res) => {
+    res.render('success');
+});
 
 app.use((req, res) => {
     res.status(404).send('Sorry, deze pagina kon ik niet vinden.');
