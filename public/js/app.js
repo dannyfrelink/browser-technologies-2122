e256275a26c93bcb7f891bf53c59a9381bca6298
dannyfrelink <danny-frelink@hotmail.com>
2022-03-31 15:02:48 +0200

    let users choose which page to edit answers

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+const changeAnswersSelect = document.querySelector('select');
+const changeAnswersButton = document.querySelector('#change_answers_button');
+
+if(changeAnswersSelect) {
+    changeAnswersButton.setAttribute('href', changeAnswersSelect.value);
+
+    changeAnswersSelect.addEventListener('change', () => {
+        changeAnswersButton.setAttribute('href', changeAnswersSelect.value);
+    });
+}
