68fe4bf54bcea348e45f48feca96894e4c018683
dannyfrelink <danny-frelink@hotmail.com>
2022-04-06 13:42:19 +0200

    only show select when there is JS

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,10 @@
+const changeAnswersLabel = document.querySelector('main label');
 const changeAnswersSelect = document.querySelector('select');
 const changeAnswersButton = document.querySelector('#change_answers_button');
 
 if(changeAnswersSelect) {
+    changeAnswersLabel.classList.remove('hidden');
+    changeAnswersSelect.classList.remove('hidden');
     changeAnswersButton.setAttribute('href', changeAnswersSelect.value);
 
     changeAnswersSelect.addEventListener('change', () => {
